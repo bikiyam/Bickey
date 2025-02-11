3e7e35591ac0b654b21c4d57797136ae189851a1
bikiyam <kihlothager@gmail.com>
2025-02-11 07:54:35 -0800

    Initial Bickey Commit

---
+++ components/navbar.vue
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+
+<template>
+    <div>
+     j
+    </div>
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+
+    <div class="w-[100vw] h-[90vh] flex text-center align-middle p-40 justify-center ">
+        <h1 class="text-[60px] text-white font-extrabold">BICKEY WAS HERE</h1>
+    </div>
+
+</template>
+<style>
+
+</style>
